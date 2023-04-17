994961aa8a37b8cf75d5c7e3b3fdae012e299791
Eduardo Aguirre <luis.24_aguirre@outlook.com>
2023-04-16 21:55:26 -0600

    camera pov and spawnLocation added

---
+++ scenes/main.gd
@@ -0,0 +1,20 @@
+extends Node
+
+@export var mob_scene: PackedScene
+
+
+
+
+func _on_mob_timer_timeout():
+	var mob = mob_scene.instantiate()
+	
+	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
+	mob_spawn_location.progress_ratio = randf()
+	#randf() produces a random value between 0 and 1
+	
+	var player_position = $Player.position
+	mob.initialize(mob_spawn_location.position, player_position)
+	
+	# Spawn the mob by adding it to the Main scene.
+	#add_child(mob)
+	
---
+++ scenes/mob.gd
@@ -0,0 +1,28 @@
+extends CharacterBody3D
+
+#min and max velocity in meter per second
+@export var min_speed = 10
+@export var max_speed = 18
+
+#physics_process works as process but with physics objects
+func _physics_process(delta):
+	move_and_slide()
+
+#func will be called from main
+func initialize(start_position, player_position):
+	# We position the mob by placing it at start_position
+	# and rotate it towards player_position, so it looks at the player.
+	look_at_from_position(start_position, player_position, Vector3.UP)
+	# Rotate this mob randomly within range of -90 and +90 degrees,
+	# so that it doesn't move directly towards the player.
+	rotate_y(randf_range(-PI/4, PI/4))
+	
+	#calculated the speed in integer
+	var random_speed = randi_range(min_speed, max_speed)
+	velocity = Vector3.FORWARD * random_speed
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
+	
+
+
+func _on_visible_on_screen_notifier_3d_screen_exited():
+	queue_free() #This function destroy the instance it's called on.
